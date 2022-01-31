312553b13b3ec216bf6b7c036a599721891deb4c
Ashutosh Thorat <ashutosh.thorat@bridgelabz.com>
2022-01-31 08:54:20 +0530

    added basic java program

---
+++ src/com/bridgelabz/javabasics/JavaBasicDemo.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.javabasics;
+
+public class JavaBasicDemo {
+	
+	public static void main(String[] args) {
+		System.out.print("hello bridgelabz!!");
+		JavaBasicDemo javabasicdemo=new JavaBasicDemo();
+		javabasicdemo.displayHello();
+	}
+	public void displayHello() {
+		System.out.print("inside display hello!!");
+		
+	}
+
+}
